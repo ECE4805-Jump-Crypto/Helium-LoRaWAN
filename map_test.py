661bc53902ab8fa77dfccfa49500c706d9e81eff
Chris Forgash <cforgash99@vt.edu>
2022-09-16 10:19:29 -0400

    fixed api limiting issue

--- map_test.py
+++ map_test.py
@@ -3,6 +3,7 @@ from flask import Flask
 import folium
 import json
 import requests
+import time
 
 
 """
@@ -10,18 +11,29 @@ The following block of code pulls the nearby node data at the given distance fro
 longitude. The distance should be kept at 12000 meters but the latitude and longitude can be adjusted for
 the proposed node location.
 """
-lat = 44.79806735325452
-lon = -106.95872819992391
-dist = 12000 #distance in meters
-url = "https://api.helium.io/v1/hotspots/location/distance?lat={}&lon={}&distance={}".format(lat,lon,dist)
-
+lat = 41.881832
+lon = -87.623177
+dist = 20000 #distance in meters
+url = "https://api.helium.io/v1/hotspots/location/distance?lat={}&lon={}&distance={}&page[size]=1000".format(lat,lon,dist)
+orig_url = url
 #defauilt payload and headers
 payload = {}
 headers = {}
+#List to hold each api request
+total_data = []
+while url:
+    time.sleep(0.1)
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.json()
+    total_data.append(data)
 
-response = requests.request("GET", url, headers=headers, data=payload)
-data = response.json()
-num_nodes = len(data["data"])
+    url = orig_url #reset url string to have no cursor
+    try:
+        cursor = data['cursor']
+        url += "&cursor={}".format(cursor)
+    except:
+        url = None
+
 
 """
 The following block of code establishes a Flask application endpoint for the folium coverage map.
@@ -38,7 +50,7 @@ def index():
     folium_map = folium.Map(location=start_coords, zoom_start=12)
 
     #rural circle bound
-    folium.Circle([lat,lon], radius=12000, color='yellow').add_to(folium_map)
+    folium.Circle([lat,lon], radius=20000, color='yellow').add_to(folium_map)
     #urban circle bound
     folium.Circle([lat,lon], radius=5000, color='green').add_to(folium_map)
 
@@ -49,17 +61,18 @@ def index():
                     fill_color='blue').add_to(folium_map)
 
     #nearby node markers
-    for i in range(0,num_nodes-1):
-        if data["data"][i]["status"]["online"] == "online":
-            status_color = 'green'  # green marker indicates online node
-        else:
-            status_color = 'red'    # red marker indicates offline node
+    for j in range(len(total_data)):
+        for i in range(len(total_data[j]["data"])):
+            if total_data[j]["data"][i]["status"]["online"] == "online":
+                status_color = 'green'  # green marker indicates online node
+            else:
+                status_color = 'red'    # red marker indicates offline node
 
-        folium.CircleMarker([data["data"][i]["lat"],data["data"][i]["lng"]],
-                        radius=5,
-                        color=status_color,
-                        fill_color=status_color,
-                        popup=data["data"][i]["name"]).add_to(folium_map)
+            folium.CircleMarker([total_data[j]["data"][i]["lat"],total_data[j]["data"][i]["lng"]],
+                            radius=5,
+                            color=status_color,
+                            fill_color=status_color,
+                            popup=total_data[j]["data"][i]["name"]).add_to(folium_map)
 
     return folium_map._repr_html_()
 
